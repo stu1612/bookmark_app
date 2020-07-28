d0b51d6336f727f49e61a6a31540054fc8d6b9ce
Stu Bolderson <stu.bolderson@aol.com>
2020-07-28 15:32:52 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// UI Vars
+const btn = document.getElementById('button');
+
+
+// Functions
+buttonClick = () => {
+    console.log('Button has been clicked !');
+}
+
+
+
+// Event Listeners
+btn.addEventListener('click', buttonClick)
